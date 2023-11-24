469cf880414e212298975487adfeb50910da6de0
Gabriel Vieira <gabriel.vieira@autoglass.com.br>
2023-11-24 13:23:57 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // São as importações
+
+namespace MeuApp // Divisões lógicas
+{
+    class program // É o programa em si,tudo que será executado na aplicação.
+    {
+        static void Main(string[] args) // Método main, é o principal método da aplicação, é o que será executado primeiro  
+        {
+            var texto = "O Gabriel é o melhor do mundo! RECEBAAAA!!";
+            Console.WriteLine(texto);
+        }
+    }
+}
