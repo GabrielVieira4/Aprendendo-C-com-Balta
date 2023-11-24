6d982e5147e26879415c0de19b8a8826cb3433f4
Gabriel Vieira <gabriel.vieira@autoglass.com.br>
2023-11-24 16:44:50 -0300

    Aprendendo sobre parametros

--- Program.cs
+++ Program.cs
@@ -4,10 +4,22 @@ namespace MeuApp // Divisões lógicas
 {
     class program // É o programa em si,tudo que será executado na aplicação.
     {
-        static void Main(string[] args) // Método main, é o principal método da aplicação, é o que será executado primeiro  
+        static void Main(string[] args) // Método main, é o principal método da aplicação, é o que será executado primeiro
         {
-            var texto = "O Gabriel é o melhor do mundo! RECEBAAAA!!";
-            Console.WriteLine(texto);
+            string nome = RetornaNome("Gabriel", "Vascaino", 27);
+            Console.WriteLine(nome);
+
+            MeuMetodo();
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("Calma Largatixo!");
+        }
+
+        static string RetornaNome(string nome, string sobrenome, int idade)
+        {
+            return nome + " " + sobrenome + " tem " + idade.ToString() + " " + "anos";
         }
     }
 }
